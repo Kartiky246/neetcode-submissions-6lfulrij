ca1e25b32eddb7aa6145627b015a59918daa49f8
Kartikey <69628973+Kartiky246@users.noreply.github.com>
2026-04-25 15:31:20 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const numberSet = new Set();
+        for(let i = 0; i < nums.length; i++){
+            if(numberSet.has(nums[i])) return true;
+            numberSet.add(nums[i]);
+        }
+        return false;
+    }
+}

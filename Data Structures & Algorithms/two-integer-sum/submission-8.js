14e09778d30397c4efcfeff565d59a294bc29608
Kartikey <69628973+Kartiky246@users.noreply.github.com>
2026-04-25 18:50:39 +0530

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const numberMap = new Map();
+        for(let i =0; i<nums.length; i++){
+            if(numberMap.has(target - nums[i])){
+                return [numberMap.get(target - nums[i]), i]
+            }
+            numberMap.set(nums[i], i);
+        }
+        return [-1, -1]
+    }
+}
